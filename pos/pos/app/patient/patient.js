d1a3540c618f7656f28948b700f3496267c79bab
Nagender Reddy <nagireddym2004@gmail.com>
2014-08-31 22:59:29 +0530

    Cleaned up some script and styles for autocomplete

--- pos/pos/app/patient/patient.js
+++ pos/pos/app/patient/patient.js
@@ -1,7 +1,6 @@
 ﻿'use strict';
 angular.module('pos').controller('patient', patient);
 patient.$inject = ['patientService'];
-
 function patient(patientService) {
     var title = 'Patient Search'
    
@@ -16,18 +15,38 @@ function patient(patientService) {
         title: title
     };
 
-  
+    init();
 
     return vm;
 
     function init() {
-        //uncomment the below code to get the model from the webapi. The patientService.resolve will be called by routing 
-        //which in turn fetches the records from webapi
-        patientName = patientService.model;
-       
+        var autocomplete = $("#Patients").kendoAutoComplete({
+            minLength: 1,
+            dataTextField: "PatientName",
+            headerTemplate: '<div class="dropdown-header">' +
+                    '<span class="k-widget k-header">Photo</span>' +
+                    '<span class="k-widget k-header">Contact info</span>' +
+                '</div>',
+            template: '<span class="k-state-default"><img src=\"../content/web/Customers/#:data.PatientID#.jpg\" alt=\"#:data.PatientID#\" /></span>' +
+                      '<span class="k-state-default"><h3>#: data.PatientName #</h3><p>Patient No. #: data.PatientNumber #</p><p>DOB #: data.DateOfBirth #</p></span>',
+            dataSource: {
+                transport: {
+                    read: {
+                        dataType: "json",
+                        url: "/api/Patient"
+                    }, //read
+                    parameterMap: function () {// send value of autocomplete as the "startsWith" parameter
+                        return {
+                            FirstName: $("#Patients").data("kendoAutoComplete").value()
+                        };
+                    }
+                }
+            },
+            height: 370,
+        }).data("kendoAutoComplete");
     }
    
-
+  
     function save() {
         patientService.save().success(function () {
             alert('Saved');
