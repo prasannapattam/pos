950a9c7bccffbcb7753c1998cdab0883c9ab71ea
prasannapattam <pprasannak@hotmail.com>
2014-10-21 07:09:43 +0530

    Added div headers and encounter icons

--- pos/pos/app/patient/portal.js
+++ pos/pos/app/patient/portal.js
@@ -1,19 +1,19 @@
 ﻿'use strict';
 angular.module('pos').controller('portal', portal);
-portal.$inject = ['patientService'];
-function portal(patientService) {
+portal.$inject = ['$filter', 'patientService', 'utility'];
+function portal($filter, patientService, utility) {
 
     var encounterGridOptions = {
         enableColumnResizing: true,
         columnDefs: [
-                { name: 'ExamDate', field: 'ExamDate', displayName: 'Encounter History', cellTemplate: "<span>{{getExternalScopes().getHistoryText(row)}}</span>" },
+                { name: 'ExamDate', field: 'ExamDate', displayName: 'Encounter History', enableSorting: false, disableColumnMenu: true, cellTemplate: encounterCellTemplate() },
         ]
     };
 
     var vm = {
         patientModel: {},
         encounterGridOptions: encounterGridOptions,
-        getHistoryText: getHistoryText
+        getHistoryText: getHistoryText,
     };
 
     init();
@@ -28,18 +28,27 @@ function portal(patientService) {
     }
 
     function getHistoryText(row) {
-        var text = row.entity.ExamDate;
+        var text = $filter('date')(row.entity.ExamDate, 'MM/dd/yyyy');
         if(row.entity.CorrectExamID !== null){
-            text += "(Corrected on " + ExamCorrectDate + ")";
+            text += '(Corrected on ' + $filter('date')(row.entity.ExamCorrectDate, 'MM/dd/yyyy') + ')';
         }
-        else if(row.entity.SavedInd !== 1){
-            text += "(Saved on " + LastUpdatedDate + ")";
+        else if(row.entity.SavedInd === 1){
+            text += '(Saved on ' + $filter('date')(row.entity.LastUpdatedDate, 'MM/dd/yyyy h:mm a') + ')';
         }
         return text;
         //<span data-bind="text: moment(ExamDate).format('L')"></span>
         //<span data-bind="css: {display_none : CorrectExamID === null}, text: '(Corrected on ' + moment(ExamCorrectDate).format('L') + ')'"></span>
         //<span data-bind="css: {display_none : SavedInd != 1}, text: '(Saved on ' + moment(LastUpdatedDate).format('L h:mm a') + ')'"></span>
 
+    }
+
+    function encounterCellTemplate() {
+        return "<div class='pull-left'>{{getExternalScopes().getHistoryText(row)}}</div>" 
+            + "<div class='encounter-right'>"
+            + "<img src='" + utility.virtualDirectory + "/content/images/icons/note-edit.png' title='Correct Notes' class='grid-icon' />&nbsp;&nbsp;"
+            + "<img src='" + utility.virtualDirectory + "/content/images/icons/mail.png' class='grid-icon' title='Print Letter' />&nbsp;&nbsp;"
+            + "<img src='" + utility.virtualDirectory + "/content/images/icons/printer.png' class='grid-icon' title='Print Notes' />&nbsp;&nbsp;"
+            + "</div>";
     }
 }
 
